b72292ba993a86897367aa3d56a64064130779f8
omar delarosa <thedelarosa@gmail.com>
2013-07-31 15:48:01 -0400

    added print_state method

--- hanoi.rb
+++ hanoi.rb
@@ -22,7 +22,27 @@ class Hanoi
             #add them to the starting peg
             @pegs[:from] << disc
         end
+    end
 
+    def shift(pegs={})
+        #this is going to handle our peg shifting procedure
+    end
+
+    def move
+        #this will handle our legal move procedure
+        #(note: this is the recursive call)
+
+
+    end
+
+    def print_state
+        @pegs_array.each_with_index do |peg, peg_number|
+            print "Peg #{peg_number} : "
+            peg.each do |disc|
+                print disc.to_s+", "
+            end
+            print "\n"
+        end
     end
 
 end
